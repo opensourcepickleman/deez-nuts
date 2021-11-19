2450985036f4e0e8093b3e5f5be23706a7b654e4
opensourcepickleman <94691757+opensourcepickleman@users.noreply.github.com>
2021-11-19 09:35:00 -0330

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-    char name[4];
+    char name[4]; //only answer is nuts
     printf("enter deez: ");
     scanf("%s", name);
     printf("your deez is %s.", name);
     return 0;
+    //this program is retarded
 }
